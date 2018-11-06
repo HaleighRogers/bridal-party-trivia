88d25abf4fc29b0bcd0bfd5a70e2eddfb4630370
HaleighRogers <hcr150130@utdallas.edu>
2018-11-06 10:57:45 -0600

    Started work on adding a welcome screen

---
+++ src/components/Welcome.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+import {
+    Link
+} from 'react-router-dom'
+// import Image from "../assets/WelcomeBackground.jpeg"
+
+const Welcome = () => (
+        <Link to={`/game`}>
+            // <img src={Image}/>
+        </Link>
+)
+
+export default Welcome
